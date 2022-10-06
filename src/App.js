3f355624e842e2952a427bc1e45925411eb0836c
MINALI Eros <eros7esc@hotmail.com>
2022-10-05 22:18:14 -0300

    proyecto actualizado

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos a react gato
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende mas react wachin
         </a>
       </header>
     </div>
